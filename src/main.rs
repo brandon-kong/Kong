40da1e6bb88c82d67d966a36f573bc4fd48c5dc5
brandon-kong <kongbrandon0@gmail.com>
2024-01-29 23:11:57 -0600

    Added basic command line for executing kong files

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,64 @@
+use std::fs;
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+
+    loop {
+
+        let mut line = String::new();
+        
+        // Read command in the format of "kong file_dir"
+        io::stdin().read_line(&mut line).expect("Failed to read line");
+
+        // Split the command into two parts
+        let mut iter = line.split_whitespace();
+        let command = iter.next();
+        let file_dir = iter.next();
+
+        match command {
+            Some("kong") => {
+                match file_dir {
+                    Some(dir) => {
+                        println!("Kong is running...");
+                        match fs::read_to_string(dir) {
+                            Ok(contents) => println!("{}", contents),
+                            Err(e) => eprintln!("Failed to read file: {}", e),
+                        }
+                    },
+                    None => {
+                        println!("No file directory provided. Usage: kong [file_dir]");
+                    }
+                }
+            },
+
+            Some("ls") => {
+                match file_dir {
+                    Some(dir) => {
+                        println!("Listing files in directory...");
+                        match fs::read_dir(dir) {
+                            Ok(entries) => {
+                                for entry in entries {
+                                    match entry {
+                                        Ok(entry) => println!("{}", entry.file_name().to_string_lossy()),
+                                        Err(e) => eprintln!("Failed to read directory entry: {}", e),
+                                    }
+                                }
+                            },
+                            Err(e) => eprintln!("Failed to read directory: {}", e),
+                        }
+                    },
+                    None => {
+                        println!("No directory provided. Usage: ls [dir]");
+                    }
+                }
+            },
+
+            Some(_) => {
+                println!("Invalid command. Usage: kong [file_dir]");
+            },
+            None => {
+                println!("No command provided. Usage: kong [file_dir]");
+            },
+        }
+    }
 }
